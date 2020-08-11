6276affe2b1def5bb7e5b60981492a9e4d9ba751
Jay T <jaytula@gmail.com>
2020-08-11 15:30:47 -0700

    Multiple Themes: Shadow bio-content.js

---
+++ multiple-themes/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+const BioContent = () => (
+  <Fragment>
+    Words by <Styled.a href="http://example.com/">Jay Doe</Styled.a>.
+    <br />
+    Change me. Your awesome bio, about how great you are!
+  </Fragment>
+)
+
+export default BioContent
