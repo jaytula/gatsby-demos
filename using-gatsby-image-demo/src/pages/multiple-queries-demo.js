3c79566e219a7bb9d410940785134870e8044903
Jay T <jaytula@gmail.com>
2020-09-05 14:54:27 -0700

    Gatsby Image: Multiple Queries and Aliasing - finished

--- using-gatsby-image-demo/src/pages/multiple-queries-demo.js
+++ using-gatsby-image-demo/src/pages/multiple-queries-demo.js
@@ -1,45 +1,55 @@
-import React from 'react'
-import Layout from '../components/layout'
-import Img from 'gatsby-image'
-import { graphql } from 'gatsby'
+import React from "react"
+import Layout from "../components/layout"
+import Img from "gatsby-image"
+import { graphql } from "gatsby"
 
+const MultipleQueriesDemo = ({ data }) => {
+  return (
+    <Layout>
+      <h1>Multiple Queries Demo</h1>
+      <h2>allSpeakingYaml</h2>
 
-const MultipleQueriesDemo = ({data}) => {
-  return <Layout>
-    <h1>Multiple Queries Demo</h1>
-    <h2>allSpeakingYaml</h2>
+      {data.allSpeakingYaml.edges.map(({ node }) => (
+        <Img
+          key={node.image.childImageSharp.fluid.src}
+          fluid={node.image.childImageSharp.fluid}
+          alt=""
+        />
+      ))}
+      <h2>banner</h2>
 
-    <h2>banner</h2>
+      <Img fluid={data.banner.childImageSharp.fluid} alt="" />
 
-    <pre>
-      {JSON.stringify(data, null, 2)}
-    </pre>
-  </Layout>
+      <h2>JSON</h2>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </Layout>
+  )
 }
 
 export default MultipleQueriesDemo
 
 export const query = graphql`
-{
-  allSpeakingYaml {
-    edges {
-      node {
-        image {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
+  {
+    allSpeakingYaml {
+      edges {
+        node {
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
             }
           }
         }
       }
     }
-  }
-  banner: file(relativePath: { eq: "strawberry-jam.jpg" }) {
-    childImageSharp {
-      fluid {
-        ...GatsbyImageSharpFluid
+    banner: file(relativePath: { eq: "strawberry-jam.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
       }
     }
   }
-}
 `
