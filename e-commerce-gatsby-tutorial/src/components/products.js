5d47f3f7f1cf8198b438fb44bdebddf1665840f3
Jay T <jaytula@gmail.com>
2020-09-19 11:03:52 -0700

    E-commerce Stripe: Create a component that represents a single product

--- e-commerce-gatsby-tutorial/src/components/products.js
+++ e-commerce-gatsby-tutorial/src/components/products.js
@@ -1,33 +1,46 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import ProductCard from "./Products/ProductCard"
 
 const query = graphql`
-{
-  allStripePrice(filter: {active: {eq: true}}, sort: {fields: unit_amount}) {
-    edges {
-      node {
-        id
-        product {
-          name
+  {
+    allStripePrice(
+      filter: { active: { eq: true } }
+      sort: { fields: unit_amount }
+    ) {
+      edges {
+        node {
           id
+          product {
+            name
+            id
+          }
+          currency
+          active
+          unit_amount
         }
-        currency
-        active
-        unit_amount
       }
     }
   }
-}
-
 `
 
 const Products = () => {
   const data = useStaticQuery(query)
+
+  const products = {}
+  for (const { node } of data.allStripePrice.edges) {
+    const product = node.product
+    if (!products[product.id]) {
+      products[product.id] = product
+      products[product.id].prices = []
+    }
+    products[product.id].prices.push(node)
+  }
   return (
     <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      {data.allStripePrice.edges.map(({ node }) => (
-        <p key={node.id}>{node.product.name}</p>
+      {/* <pre>{JSON.stringify(data.allStripePrice.edges, null, 2)}</pre> */}
+      {Object.keys(products).map(key => (
+        <ProductCard key={products[key].id} product={products[key]} />
       ))}
     </div>
   )
