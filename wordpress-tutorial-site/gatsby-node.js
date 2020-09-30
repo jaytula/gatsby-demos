16de00e56f10c8e7ed481bf4d259c9a81e3031a3
Jay T <jaytula@gmail.com>
2020-09-30 13:08:57 -0700

    Use gatsby node api createPages to console out graphql query of all posts

--- wordpress-tutorial-site/gatsby-node.js
+++ wordpress-tutorial-site/gatsby-node.js
@@ -5,3 +5,25 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = async ({graphql}) => {
+  const result = await graphql(`
+  query MyQuery {
+  allWpPost {
+    edges {
+      node {
+        id
+        excerpt
+        content
+        date
+        slug
+        title
+      }
+    }
+  }
+}
+`);
+
+ console.log(JSON.stringify(result, null, 2));
+ return result;
+}
