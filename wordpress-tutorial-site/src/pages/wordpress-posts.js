8e82f98346181cf67ea1f14e2620a390a0e31186
Jay T <jaytula@gmail.com>
2020-09-29 17:05:33 -0700

    Rendering the blog posts to /wordpress-posts

--- wordpress-tutorial-site/src/pages/wordpress-posts.js
+++ wordpress-tutorial-site/src/pages/wordpress-posts.js
@@ -4,16 +4,16 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const WordpressBlogsPage = ({ data }) => (
+const WordpressPosts = ({ data }) => (
   <Layout>
-    <SEO title="Wordpress Blogs" />
-    <h1>Wordpress Blogs</h1>
+    <SEO title="Wordpress Posts" />
+    <h1>Wordpress Posts</h1>
     <pre>{JSON.stringify(data, null, 2)}</pre>
-    {data.allWpPage.edges.map(({ node }) => {
+    {data.allWpPost.edges.map(({ node }) => {
       return (
         <div key={node.id}>
           <h2>{node.title}</h2>
-          <div dangerouslySetInnerHTML={{ __html: node.content }} />
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
         </div>
       )
     })}
@@ -22,17 +22,19 @@ const WordpressBlogsPage = ({ data }) => (
 )
 
 export const query = graphql`
-  query MyQuery {
-    allWpPage {
-      edges {
-        node {
-          id
-          title
-          content
-        }
+query MyQuery {
+  allWpPost {
+    edges {
+      node {
+        id
+        excerpt
+        slug
+        title
       }
     }
   }
+}
+
 `
 
-export default WordpressBlogsPage
+export default WordpressPosts
