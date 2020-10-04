4f1aef7cf84dd170c2e281082dd37a2c29465e48
Jay T <jaytula@gmail.com>
2020-10-04 16:16:49 -0700

    Wordpress Images: Rendering the images to index.js

--- images-tutorial-site/src/pages/index.js
+++ images-tutorial-site/src/pages/index.js
@@ -1,22 +1,69 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      {data.allWpPost.edges.map(({ node }) => {
+        const imageRes = node.featuredImage.node.localFile.childImageSharp.fixed
+        return <Img fixed={imageRes} key={imageRes.src} />
+      })}
+      <div>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </Layout>
+  )
+}
+
+// Documentation says to use fragment: ...GatsbyImageSharpFixed_withWebp_tracedSVG
+// But we explicitly try the fields for clarity instead
+
+export const query = graphql`
+  query MyQuery {
+    allWpPost {
+      edges {
+        node {
+          id
+          content
+          date
+          slug
+          title
+          featuredImageId
+          featuredImage {
+            node {
+              localFile {
+                childImageSharp {
+                  fixed(height: 200, width: 200) {
+                    srcWebp
+                    srcSetWebp
+                    srcSet
+                    src
+                    width
+                    tracedSVG
+                    height
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
