373c3c693b678537ee76d70c7ff7efb4707aa6e6
Jay T <jaytula@gmail.com>
2020-10-06 17:19:20 -0700

    Prismic: Access all posts with Graphql query

--- gatsby-prismic-blog/src/pages/index.js
+++ gatsby-prismic-blog/src/pages/index.js
@@ -4,19 +4,34 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>Home Page</h1>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
   </Layout>
 )
 
-export default IndexPage
+export const query = graphql`
+query MyQuery {
+  allPrismicPost {
+    edges {
+      node {
+        data {
+          body {
+            html
+            text
+          }
+          title {
+            html
+            text
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+export default IndexPage;
