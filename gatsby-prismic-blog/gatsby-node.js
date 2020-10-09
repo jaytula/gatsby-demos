620b112445e68f39c852cb4050fc2bfbb171d2eb
Jay T <jaytula@gmail.com>
2020-10-09 01:22:49 -0700

    Implement exports.createPages

--- gatsby-prismic-blog/gatsby-node.js
+++ gatsby-prismic-blog/gatsby-node.js
@@ -5,3 +5,43 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path');
+
+exports.createPages = async({graphql, actions}) => {
+  const { createPage } = actions;
+
+  const pages = await graphql(`
+  query MyQuery {
+    allPrismicPost {
+      nodes {
+        id
+        uid
+        data {
+          body {
+            html
+            raw
+            text
+          }
+          title {
+            html
+            raw
+            text
+          }
+        }
+      }
+    }
+  }
+  `);
+
+  pages.data.allPrismicPost.nodes.forEach(node => {
+    createPage({
+      path: `/${node.uid}`,
+      component: path.resolve(__dirname, 'src/templates/post.js'),
+      context: {
+        id: node.id,
+        uid: node.uid
+      }
+    })
+  })
+}
