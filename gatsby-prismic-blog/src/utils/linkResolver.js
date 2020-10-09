897440232d76b3eb8faaca077821a58e6b9c3bb2
Jay T <jaytula@gmail.com>
2020-10-09 01:05:27 -0700

    Remove linkResolver.js

--- gatsby-prismic-blog/src/utils/linkResolver.js
+++
@@ -1,8 +0,0 @@
-exports.linkResolver = function linkResolver(doc) {
-  // Route for blog posts
-  if (doc.type === 'post') {
-    return "/blog/" + doc.uid
-  }
-  // Homepage route fallback
-  return "/"
-}
