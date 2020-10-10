3037367a02a30c91e65788907776b99201e354b9
Jay T <jaytula@gmail.com>
2020-10-10 10:20:28 -0700

    Prismic Preview: Setup wrapRootElement to be wrapped by PreviewStoreProvider

--- gatsby-prismic-blog/README.md
+++ gatsby-prismic-blog/README.md
@@ -1,2 +1,9 @@
 - Use this plugin instead: https://github.com/angeloashmore/gatsby-source-prismic
 - Tutorial Link: https://www.gatsbyjs.com/tutorial/prismic-source-graphql-tutorial/
+
+## Gatsby Prismic Preview Notes
+
+### PreviewStoreProvider
+
+- See here:
+- Setup wrapRootElement in both `gatsby-ssr.js` and `gatsby-browser.js`
--- gatsby-prismic-blog/gatsby-browser.js
+++ gatsby-prismic-blog/gatsby-browser.js
@@ -1,7 +1,6 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
+import * as React from "react"
+import { PreviewStoreProvider } from "gatsby-source-prismic"
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => {
+  return <PreviewStoreProvider>{element}</PreviewStoreProvider>
+}
