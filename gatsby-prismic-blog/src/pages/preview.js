204b92fd51a9fbd561d7b8137c8cd13fef34da43
Jay T <jaytula@gmail.com>
2020-10-14 16:17:33 -0700

    Add todo paragraph in preview.js

--- gatsby-prismic-blog/src/pages/preview.js
+++ gatsby-prismic-blog/src/pages/preview.js
@@ -5,13 +5,17 @@ import { linkResolver } from "../linkResolver"
 
 import Layout from "../components/layout"
 
-const PreviewPage = (props) => {
-  const {isPreview, data, isLoading } = props
+const PreviewPage = props => {
+  const { isPreview, data, isLoading } = props
   if (isPreview === false) return "Not a preview!"
 
   return (
     <Layout>
       <h1>Preview Page</h1>
+      <p>
+        TODO: This page then determines the previewed document's URL and redirects to
+        that page while storing the preview data.
+      </p>
       <pre>{JSON.stringify(props, null, 2)}</pre>
     </Layout>
   )
@@ -20,4 +24,3 @@ const PreviewPage = (props) => {
 export default withPreviewResolver(PreviewPage, {
   repositoryName: process.env.GATSBY_PRISMIC_REPOSITORY_NAME,
 })
-
