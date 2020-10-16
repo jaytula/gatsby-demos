20ca3037db96dc026f9b2d3d161f6b320375efc8
Jay T <jaytula@gmail.com>
2020-10-16 14:10:56 -0700

    Implement linkResolver

--- gatsby-prismic-blog/src/linkResolver.js
+++ gatsby-prismic-blog/src/linkResolver.js
@@ -1,1 +1,9 @@
-export const linkResolver = () => {}
+export const linkResolver = ({node: doc}) => {
+  console.log(doc);
+  // Pretty URLs for known types
+  if (doc.type === 'blog') return "/post/" + doc.uid;
+  if (doc.type === 'page') return "/" + doc.uid;
+  // Fallback for other types, in case new custom types get created
+  console.log('got here');
+  return "/doc/" + doc.id;
+};
