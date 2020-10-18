8dafdfb189dfb894bdd9992436e8bfc2f2662cdf
Jay T <jaytula@gmail.com>
2020-10-18 10:09:32 -0700

    Update Doc component to match the withPreview documentation more closely

--- gatsby-prismic-blog/src/templates/doc.js
+++ gatsby-prismic-blog/src/templates/doc.js
@@ -1,7 +1,31 @@
+import React from "react"
 import { withPreview } from "gatsby-source-prismic"
-import Post from './post';
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
 // TODO: how can this component respond to /doc/[id] instead of just /doc
 
-const Doc = withPreview(Post)
-export default Doc
+const Doc = ({ data }) => {
+  return (
+    <Layout>
+      <p>Doc component</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </Layout>
+  )
+}
+export default withPreview(Doc)
+
+export const query = graphql`
+  query DocTemplate($uid: String!) {
+    prismicPost(uid: { eq: $uid }) {
+      data {
+        title {
+          text
+        }
+        body {
+          text
+        }
+      }
+    }
+  }
+`
