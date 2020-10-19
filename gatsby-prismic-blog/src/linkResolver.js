a8db2c9789d16892f3e4478a083688d46205eeab
Jay T <jaytula@gmail.com>
2020-10-19 06:34:54 -0700

    Update linkResolver to return a function

--- gatsby-prismic-blog/src/linkResolver.js
+++ gatsby-prismic-blog/src/linkResolver.js
@@ -1,10 +1,8 @@
 export const linkResolver = ({node: doc}) => {
-  console.log(doc);
   // Pretty URLs for known types
-  if (doc.type === 'blog') return "/post/" + doc.uid;
-  if (doc.type === 'page') return "/" + doc.uid;
+  if (doc.type === 'blog') return () => "/post/" + doc.uid;
+  if (doc.type === 'page') return () => "/" + doc.uid;
   // Fallback for other types, in case new custom types get created
-  console.log('got here');
 
-  return "/doc/" + doc.uid;
+  return () => "/doc/" + doc.uid;
 };
