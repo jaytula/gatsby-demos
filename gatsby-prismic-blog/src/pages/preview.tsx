7dc8b1f0799564d2fdafeb23c1708c0346ff801b
Jay T <jaytula@gmail.com>
2020-10-20 10:10:37 -0700

    Convert preview.js to typescript
    This helps with getting linkResolver right

--- gatsby-prismic-blog/src/pages/preview.tsx
+++ gatsby-prismic-blog/src/pages/preview.tsx
@@ -7,14 +7,14 @@ import Layout from "../components/Layout"
 
 const PreviewPage = props => {
   const { isPreview } = props
-  if (isPreview === false) return "Not a preview!"
+  if (isPreview === false) return <p>"Not a preview!"</p>
 
   return (
     <Layout>
       <h1>Preview Page</h1>
       <p>
-        TODO: This page then determines the previewed document's URL and redirects to
-        that page while storing the preview data.
+        TODO: This page then determines the previewed document's URL and
+        redirects to that page while storing the preview data.
       </p>
       <pre>{JSON.stringify(props, null, 2)}</pre>
     </Layout>
@@ -23,5 +23,5 @@ const PreviewPage = props => {
 
 export default withPreviewResolver(PreviewPage, {
   repositoryName: process.env.GATSBY_PRISMIC_REPOSITORY_NAME,
-  linkResolver
+  linkResolver,
 })
