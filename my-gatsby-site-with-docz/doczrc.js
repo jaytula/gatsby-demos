7679a9fd546747c2d54e8cb04070077695f58b8d
Jay T <jaytula@gmail.com>
2020-10-22 14:27:00 -0700

    Add doczrs.js

---
+++ my-gatsby-site-with-docz/doczrc.js
@@ -0,0 +1,23 @@
+  
+export default {
+  title: "Gatsby Docz Demooo",
+  themeConfig: {
+    initialColorMode: "dark",
+    colors: {
+      modes: {
+        light: {
+          header: {
+            button: {},
+          },
+        },
+        dark: {
+          header: {
+            button: {
+              // bg: "#7ba7ff",
+            },
+          },
+        },
+      },
+    },
+  },
+}
