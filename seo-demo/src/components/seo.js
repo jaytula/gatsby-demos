bd3c3de56c8116a1812df39bad2030220496dd57
Jay T <jaytula@gmail.com>
2020-11-01 18:20:27 -0800

    Add SEO props and prpTypes

--- seo-demo/src/components/seo.js
+++ seo-demo/src/components/seo.js
@@ -1,8 +1,9 @@
 import React from "react"
+import PropTypes from 'prop-types';
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = ({ description }) => {
+const SEO = ({ description, lang, meta, image: metaImage, title }) => {
   const { site } = useStaticQuery(
     grqphql`
       query {
@@ -41,6 +42,18 @@ SEO.defaultProps = {
   lang: "en",
   meta: [],
   description: "",
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+  image: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired
+  })
 }
 
 export default SEO
