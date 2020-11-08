5b71624b85fab3c6311ba7ea6d45f67c93c901bd
Jay T <jaytula@gmail.com>
2020-11-08 01:47:15 -0800

    Change BlogPostTemplate to a function component

--- seo-demo/src/templates/blog-post.js
+++ seo-demo/src/templates/blog-post.js
@@ -4,31 +4,30 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
+const BlogPostTemplate = props => {
+  console.log({props});
+  const post = props.data.markdownRemark
 
-    // TODO: fix somehow
-    if(!post) return <div>post is null</div>
+  // TODO: fix somehow
+  if (!post) return <div>post is null</div>
 
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const image = post.frontmatter.image
-      ? post.frontmatter.image.childImageSharp.resize
-      : null
+  const siteTitle = props.data.site.siteMetadata.title
+  const image = post.frontmatter.image
+    ? post.frontmatter.image.childImageSharp.resize
+    : null
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-          image={image}
-          pathname={this.props.location.pathname}
-        />
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </Layout>
-    )
-  }
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+        image={image}
+        pathname={props.location.pathname}
+      />
+      <h1>{post.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </Layout>
+  )
 }
 
 export default BlogPostTemplate
